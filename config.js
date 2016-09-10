cc825f703abc7bbf1876158f526c4eb45c2d83d6
Dalimil Hajek <dalimilhajek@gmail.com>
2016-09-10 12:20:15 +0100

    Update port initialization

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /** This file would normally NOT be commited to the public repo */
 
 module.exports = {
-	PORT: 8080,
+	PORT: process.env.PORT || 8080,
 	APP_SECRET: 'vxZpt1uRYg6fdGsSotnksYRVnh5',
 
 	MONGODB_URI: 'mongodb://localhost:27017/myproject',
