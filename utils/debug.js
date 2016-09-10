865dd52afcd4a58a68bdffb00592281b3f04490f
Dalimil Hajek <dalimilhajek@gmail.com>
2016-09-10 12:49:47 +0100

    Update debug module

--- utils/debug.js
+++ utils/debug.js
@@ -2,6 +2,9 @@
 
 function getRequestInfo(req) {
 	return {
+		timestamp: Date.now(),
+		time: new Date().toLocaleTimeString(),
+		timedate: new Date().toLocaleString(),
 		method: req.method, // GET
 		path: req.path,		// /user/alice
 		url: req.url,		// /user/alice?search=love
@@ -11,17 +14,17 @@ function getRequestInfo(req) {
 		auth: req.user, 	// passport authentication object
 		body: req.body,		// {} <- key-values for form POST or JSON
 		headers: req.headers, // { host: 'localhost:8080', ... }
+		secure: req.secure, 	// https ? true/false
 		ip: req.ip,	
-		secure: req.secure 	// https ? true/false
+		remoteAddress: req.connection.remoteAddress, // should be same as req.ip
+		forwardedFor: req.headers['x-forwarded-for'] // original IP when proxying
 	};
 }
 
 exports.requestInfo = function(req, res, next) {
 	// The following vars will be accessible everywhere via the request object
 	req.requestInfo = getRequestInfo(req);
-	req.requestDate = Date.now();
-	req.requestTime = new Date().toLocaleTimeString();
-	// console.log(req.requestTime + " - " + req.method + " " + req.url);
+	// console.log(req.requestInfo.time + " - " + req.method + " " + req.url);
 	res.jsonPretty = function(data) {
 		this.end(JSON.stringify(data, null, 2)); // specify stringify() whitespace
 	};
