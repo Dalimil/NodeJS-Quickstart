eb5b16dda7ae64fdee331289e80509f445a62bbb
Dalimil Hajek <dalimilhajek@gmail.com>
2017-02-04 18:43:25 +0000

    Remove React demos

--- src/server/main.js
+++ src/server/main.js
@@ -40,15 +40,6 @@ app.use(session({
 	store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 
-/** Set up periodic tasks */
-const scheduler = require('./utils/scheduler');
-// use scheduleOnce() or schedule() for one-off or repeated tasks respectively
-let scheduledAlert = scheduler.schedule(30000, () => {
-	let currentTime = new Date().toLocaleTimeString();
-	console.log("~ " + currentTime); 
-});
-scheduledAlert.cancel();
-
 /** Route handlers */
 const userController = require('./controllers/users');
 const sockets = require('./controllers/sockets');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	entry: ['./src/client/scripts/main.js', './src/client/scripts/react-demo/index.jsx'],
+	entry: ['./src/client/scripts/main.js'],
 	output: {
 		path: path.resolve(__dirname, './src/client/public/build'), // absolute path
 		filename: 'bundle.js'
@@ -14,13 +14,8 @@ module.exports = {
 				exclude: /node_modules/,
 				loader: "babel-loader",
 				query: {
-					presets: ["react", "latest"]
+					presets: ["latest"]
 				}
-			},
-			// CSS modules
-			{
-				test: /\.css$/,
-				loader: "style-loader!css-loader"
 			}
 		]
 	},
